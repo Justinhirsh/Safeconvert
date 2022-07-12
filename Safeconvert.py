2d6132e597007cecb689f29e3063f84bd0909efd
Doggomakesvids <108744039+Doggomakesvids@users.noreply.github.com>
2022-07-12 17:02:48 -0400

    added more file formats

--- Safeconvert.py
+++ Safeconvert.py
@@ -3,8 +3,10 @@ from pytube import YouTube
 import os
 from tkinter import filedialog
 print("Welcome to Safeconvert")
-format = input("Please enter .mp3 or .mp4: ")
-if format == ".mp3":
+type_of_vid = input("Please enter audio or video depending on what kind of file you want")
+format = input("Please enter your desired format (ex: .mp3 for audio or .mp4 for video): ")
+
+if type_of_vid == "audio" or "Audio":
     desired_video = input("please input the link to your desired video: ")
     yt = YouTube(desired_video)
     stream = yt.streams.filter(only_audio=True).first()
@@ -12,20 +14,23 @@ if format == ".mp3":
     download_path = filedialog.askdirectory()
     out_file = stream.download(output_path=download_path)
     base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
+    new_file = base + format
     os.rename(out_file, new_file)
     print("Complete!")
     time.sleep(1000)
+
 else:
-    if format == ".mp4":
+    if type_of_vid == "video" or "Video":
         desired_video = input("please input the link to your desired video: ")
         yt = YouTube(desired_video)
-        stream = yt.streams.get_by_itag(22)
-        print("Downloading "+yt.title)
+        print("downloading " + yt.title)
         download_path = filedialog.askdirectory()
-        stream.download(output_path=(download_path))
+        stream = yt.streams.filter().first()
+        out_file = stream.download(output_path=download_path)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + format
+        os.rename(out_file, new_file)
         print("Complete!")
         time.sleep(1000)
     else:
-        if not format == ".mp3" or ".mp4":
-            print("not valid format, please enter .mp3 or .mp4")
+        print("you entered" + type_of_vid + ",please enter video or audio")
