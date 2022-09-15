08348c92613e8d6a16fbfe9f7c47368dc79970d9
Doggomakesvids <108744039+Doggomakesvids@users.noreply.github.com>
2022-09-14 19:27:24 -0500

    fixed conversion issue

--- Safeconvert.py
+++ Safeconvert.py
@@ -1,36 +1,42 @@
-import time
+import os, time, ctypes, sys,ffmpy,shutil
 from pytube import YouTube
-import os
 from tkinter import filedialog
-print("Welcome to Safeconvert")
-type_of_vid = input("Please enter audio or video depending on what kind of file you want: ")
-format = input("Please enter your desired format (ex: .mp3 for audio or .mp4 for video): ")
 
-if type_of_vid == "audio" or "Audio":
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    print("Welcome to Safeconvert")
+    format = input("Please enter your desired format (ex: mp3 for audio or mp4 for video): ")
+    type_of_vid = input("Please enter if your video is a video or audio: ")
     desired_video = input("please input the link to your desired video: ")
     yt = YouTube(desired_video)
-    stream = yt.streams.filter(only_audio=True).first()
+    yt.title = yt.title.replace(" ", "_")
     print("downloading " + yt.title)
     download_path = filedialog.askdirectory()
-    out_file = stream.download(output_path=download_path)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + format
-    os.rename(out_file, new_file)
+    if type_of_vid == "audio" or "Audio":
+        stream = yt.streams.filter(only_audio= True).first()
+    else:
+        stream = yt.streams.filter().first()
+    stream.download()
+    
+    ff = ffmpy.FFmpeg(
+        inputs={os.path.dirname(yt.title + ".mp4") + yt.title + ".mp4": None},
+        outputs={os.path.dirname(yt.title + ".mp4") + yt.title + format: None}
+    )
+    ff.run()
+    os.remove(os.path.dirname(yt.title + ".mp4") + yt.title + ".mp4")
+    shutil.move(os.path.dirname(yt.title + ".mp4") + yt.title + format, download_path)
     print("Complete!")
     time.sleep(1000)
+    input("")
+
 
 else:
-    if type_of_vid == "video" or "Video":
-        desired_video = input("please input the link to your desired video: ")
-        yt = YouTube(desired_video)
-        print("downloading " + yt.title)
-        download_path = filedialog.askdirectory()
-        stream = yt.streams.filter().first()
-        out_file = stream.download(output_path=download_path)
-        base, ext = os.path.splitext(out_file)
-        new_file = base + format
-        os.rename(out_file, new_file)
-        print("Complete!")
-        time.sleep(1000)
-    else:
-        print("you entered" + type_of_vid + ",please enter video or audio")
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+is_admin()
+
+
