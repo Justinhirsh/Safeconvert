2a6ce0d643e6f7b05b50bf3f20d9e027da6c6aa9
Doggomakesvids <108744039+Doggomakesvids@users.noreply.github.com>
2022-11-13 17:39:09 -0600

    updated main script

--- Safeconvert.py
+++ Safeconvert.py
@@ -1,4 +1,3 @@
-
 import os, time,ffmpy,shutil
 import pathlib
 from pytube import YouTube
@@ -7,17 +6,26 @@ from tkinter import filedialog
 
 
 print("Welcome to Safeconvert")
-format = input("Please enter your desired format (ex: mp3 for audio or mp4 for video): ")
+format = str(input("Please enter your desired format (ex: mp3 for audio or mp4 for video): "))
 type_of_vid = input("Please enter if your video is a video or audio: ")
 desired_video = input("please input the link to your desired video or playlist: ")
 is_playlist = input("are you converting a video or playlist: ")
 dir_path = str(pathlib.Path(__file__).resolve().parent) + "\\"
+forbiden_characters = str("/, \, <, >, :, \", |, ?, *, '")
 if is_playlist == "playlist":
     p = Playlist(desired_video)
     download_path = filedialog.askdirectory()
     print("downloading playlist")
+    for yt in p.videos:
+        if type_of_vid == "audio" or "Audio":  
+            yt.streams.filter(only_audio= True, bitrate= 320, resolution= 1080)
+        else:
+            yt.streams.filter(bitrate= 320, resolution= 1080)
+        print("filtered " + yt.title)  
     for video in p.videos:
         name = video.title.replace(" ", "_")
+        if forbiden_characters in name:
+            name = name.replace(forbiden_characters, "")
         video.streams.first().download(filename= name)
         os.rename(name, name + ".mp4")
         print("processed " + name)
@@ -25,6 +33,8 @@ if is_playlist == "playlist":
     
     for video in p.videos:
         name = video.title.replace(" ", "_")
+        if name in forbiden_characters:
+            name = name.replace(forbiden_characters, "")
         ff = ffmpy.FFmpeg(
             inputs={dir_path + name + ".mp4": None},
             outputs={dir_path + name + format: None}
@@ -37,6 +47,8 @@ if is_playlist == "playlist":
 if is_playlist == "video":
     yt = YouTube(desired_video)
     yt.title = yt.title.replace(" ", "_")
+    if forbiden_characters in yt.title:
+        yt.title = yt.title.replace(forbiden_characters, "")
     print("downloading " + yt.title)
     download_path = filedialog.askdirectory()
     if type_of_vid == "audio" or "Audio":
@@ -44,11 +56,14 @@ if is_playlist == "video":
     else:
         stream = yt.streams.filter().first()
     stream.download()
-    ff = ffmpy.FFmpeg(
-        inputs={dir_path + yt.title + ".mp4": None},
-        outputs={dir_path + yt.title + format: None}
-    )
-    ff.run()
+    try:
+        ff = ffmpy.FFmpeg(
+            inputs={dir_path + yt.title + ".mp4": None},
+            outputs={dir_path + yt.title + format: None}
+        )
+        ff.run()
+    except:
+        print("conversion failed, could be due to file format already being .mp4")
     os.remove(dir_path + yt.title + ".mp4")
     shutil.move(dir_path + yt.title + format, download_path)
     print("Complete!")
